91e1fc19a8808f2486b7c5b18a4d0e87ef71ca9f
vikki <Vikram.singh85@nic.in>
2026-03-10 12:59:41 +0530

    generate build

--- src/app/ask-ai/ask-ai.component.ts
+++ src/app/ask-ai/ask-ai.component.ts
@@ -12,7 +12,8 @@ export class AskAiComponent {
   video: any;
   transcript: any;
   apiStatus: string = 'First Ask your query?';
-  baseUrl = 'https://youtubehelper-backend-1.onrender.com';
+  baseUrl = 'https://youtubehelper-backend-2.onrender.com';
+  // https://youtubehelper-backend-2.onrender.com
   // baseUrl = 'http://localhost:8000';
 
   constructor(private http: HttpClient) { }
